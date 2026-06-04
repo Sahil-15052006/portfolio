49c5b132725d7f0a58b9d75b769cd856e9ebca0c
Sahil Sawant <sahilsawant15052006@gmail.com>
2026-06-04 05:44:25 +0000

    fixed project card min width (5)

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -2,6 +2,7 @@
 import { MoveRight } from "lucide-react";
 import ProjectItem from "../components/ProjectItem";
 import { usePortfolio } from "../context/PortfolioContext";
+import { motion } from "framer-motion";
 
 export default function Projects() {
 
@@ -14,18 +15,22 @@ export default function Projects() {
 
       <div className=" text-(--secondary) sm:text-(--secondary) text-md sm:text-2xl font-light text-center px-5">A showcase of my work</div>
 
-      <div
-        className="flex flex-row overflow-x-auto overflow-visible no-scrollbar gap-5 sm:gap-3 w-full py-5 sm:py-10 transition duration-500">
+      <motion.div
+        transition={{
+          duration: 0.5,
+          ease: "easeInOut",
+        }}
+        className="flex flex-row overflow-x-auto overflow-visible no-scrollbar gap-5 sm:gap-1 w-full py-5 sm:py-10 transition ">
             {
               projects?.map((project)=>
               <div
               key={project._id}
-              className="min-w-100 sm:min-w-100 lg:min-w-120 flex justify-center items-center transition-all duration-300 ">
+              className="min-w-100 sm:min-w-100 lg:min-w-120 flex justify-center items-center transition-all duration-500 ">
                 <ProjectItem project={project}/>
               </div>
               )
             }
-      </div>
+      </motion.div>
 
       <div className="flex justify-center items-center gap-5 text-(--secondary) sm:text-(--secondary) text-md sm:text-2xl font-light text-center px-5"> <MoveRight/>  </div>
     </div>
